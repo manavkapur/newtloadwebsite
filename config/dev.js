2bad26bd16407e968cf2c76c497ae843f79ab315
Manav Kapur <62932155+manavkapur@users.noreply.github.com>
2022-05-27 00:00:07 +0530

    Delete dev.js

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    MONGOURI:"mongodb+srv://Exposys:djyOoR0pk8s5eVb8@cluster0.g9ejf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    // JWT_SECRET:process.env.JWT_SEC,
-    JWT_SECRET: "secret"
-    // SENDGRID_API:process.env.SENDGRID_API,
-    // EMAIL:process.env.EMAIL
-}
-
